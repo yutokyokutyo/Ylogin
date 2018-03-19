e36c8c1f8573461c6a361f47e108261237c9c42a
y.kondo <k.yuto.cre@gmail.com>
2018-03-19 19:04:25 +0900

    デバッグ解除

--- start.php
+++ start.php
@@ -31,8 +31,6 @@ $prompt = array(
 );
  
 // クレデンシャルインスタンス生成
-var_dump($client_id);
-exit;
 $cred = new ClientCredential( $client_id, $client_secret );
 // YConnectクライアントのインスタンス生成
 $client = new YConnectClient( $cred );
