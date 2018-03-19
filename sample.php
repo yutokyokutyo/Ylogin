d44f48be4d0fa391727095c4c1edad399d2600af
y.kondo <k.yuto.cre@gmail.com>
2018-03-19 18:40:54 +0900

    不要な記述を削除

--- sample.php
+++ sample.php
@@ -1,146 +1,50 @@
 <?php
+// start.php
 
-// YConnectライブラリ読み込み
+// YConnectライブラリ読み込み...(1)
 require("lib/YConnect.inc");
-
-// アプリケーションID, シークレッvト
+ 
+// アプリケーションID, シークレット...(2)
 $client_id     = "dj00aiZpPXE3Y1BNSGJhVmU1ZiZzPWNvbnN1bWVyc2VjcmV0Jng9YzU-";
 $client_secret = "b1gqERWZ2gabYnzIOW9DmwYLNdEXbCIYgPbSgBY0";
-
-// 各パラメータ初期化
-$redirect_uri = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["PHP_SELF"];
-
-// リクエストとコールバック間の検証用のランダムな文字列を指定してください
+ 
+// コールバックURL...(3)
+$callback_uri = "https://" . $_SERVER["SERVER_NAME"] . "/signup.php";
+// リクエストとコールバック間の検証用のランダムな文字列を指定してください...(4)
 $state = "44Oq44Ki5YWF44Gr5L+644Gv44Gq44KL77yB";
-// リプレイアタック対策のランダムな文字列を指定してください
+// リプレイアタック対策のランダムな文字列を指定してください...(5)
 $nonce = "5YOV44Go5aWR57SE44GX44GmSUTljqjjgavjgarjgaPjgabjgog=";
-
+// レスポンスタイプ...(6)
 $response_type = OAuth2ResponseType::CODE_IDTOKEN;
+// Scope...(7)
 $scope = array(
     OIDConnectScope::OPENID,
     OIDConnectScope::PROFILE,
     OIDConnectScope::EMAIL,
     OIDConnectScope::ADDRESS
 );
+// display...(8)
 $display = OIDConnectDisplay::DEFAULT_DISPLAY;
+// prompt...(9)
 $prompt = array(
     OIDConnectPrompt::DEFAULT_PROMPT
 );
-
+ 
 // クレデンシャルインスタンス生成
 $cred = new ClientCredential( $client_id, $client_secret );
-// YConnectクライアントインスタンス生成
+// YConnectクライアントのインスタンス生成
 $client = new YConnectClient( $cred );
-
-// デバッグ用ログ出力
+ 
+// デバッグ用ログ出力...(10)
 $client->enableDebugMode();
-
-try {
-
-    // Authorization Codeを取得
-    $code_result = $client->getAuthorizationCode( $state );
-
-    if( !$code_result ) {
-
-        /*****************************
-             Authorization Request    
-        *****************************/
-
-        // Authorizationエンドポイントにリクエスト
-        $client->requestAuth(
-            $redirect_uri,
-            $state,
-            $nonce,
-            $response_type,
-            $scope,
-            $display,
-            $prompt
-        );
-
-    } else {
-
-        /****************************
-             Access Token Request    
-        ****************************/
-
-        // Tokenエンドポイントにリクエスト
-        $client->requestAccessToken(
-            $redirect_uri,
-            $code_result
-        );
-
-        echo "<h1>Access Token Request</h1>";
-        // アクセストークン, リフレッシュトークン, IDトークンを取得
-        echo "ACCESS TOKEN : " . $client->getAccessToken() . "<br/><br/>";
-        echo "REFRESH TOKEN: " . $client->getRefreshToken() . "<br/><br/>";
-        echo "EXPIRATION   : " . $client->getAccessTokenExpiration() . "<br/><br/>";
-
-        /*****************************
-             Verification ID Token
-        *****************************/
-
-        // IDトークンを検証
-        $client->verifyIdToken( $nonce );
-        echo "ID TOKEN: <br/>";
-        echo "<pre>" . print_r( $client->getIdToken(), true ) . "</pre>";
-
-        /************************
-             UserInfo Request
-        ************************/
-
-        // UserInfoエンドポイントにリクエスト
-        $client->requestUserInfo( $client->getAccessToken() );
-        echo "<h1>UserInfo Request</h1>";
-        echo "UserInfo: <br/>";
-        // UserInfo情報を取得
-        echo "<pre>" . print_r( $client->getUserInfo(), true ) . "</pre>";
-
-    }
-
-} catch ( OAuth2ApiException $ae ) {
-
-    // アクセストークンが有効期限切れであるかチェック
-    if( $ae->invalidToken() ) {
-
-        /************************************
-             Refresh Access Token Request
-        ************************************/
-
-        try {
-
-            // 保存していたリフレッシュトークンを指定してください
-            $refresh_token = "STORED_REFRESH_TOKEN";
-
-            // Tokenエンドポイントにリクエストしてアクセストークンを更新
-            $client->refreshAccessToken( $refresh_token );
-            echo "<h1>Refresh Access Token Request</h1>";
-            echo "ACCESS TOKEN : " . $client->getAccessToken() . "<br/><br/>";
-            echo "EXPIRATION   : " . $client->getAccessTokenExpiration();
-
-        } catch ( OAuth2TokenException $te ) {
-
-            // リフレッシュトークンが有効期限切れであるかチェック
-            if( $te->invalidGrant() ) {
-                // はじめのAuthorizationエンドポイントリクエストからやり直してください
-                echo "<h1>Refresh Token has Expired</h1>";
-            }
-
-            echo "<pre>" . print_r( $te, true ) . "</pre>";
-
-        } catch ( Exception $e ) {
-            echo "<pre>" . print_r( $e, true ) . "</pre>";
-        }
-
-    } else if( $ae->invalidRequest() ) {
-        echo "<h1>Invalid Request</h1>";
-        echo "<pre>" . print_r( $ae, true ) . "</pre>";
-    } else {
-        echo "<h1>Other Error</h1>";
-        echo "<pre>" . print_r( $ae, true ) . "</pre>";
-    }
-
-} catch ( Exception $e ) {
-    echo "<pre>" . print_r( $e, true ) . "</pre>";
-}
-
-/* vim:ts=4:sw=4:sts=0:tw=0:ft=php:set et: */
+ 
+// Authorizationエンドポイントにリクエスト...(11)
+$client->requestAuth(
+    $callback_uri,
+    $state,
+    $nonce,
+    $response_type,
+    $scope,
+    $display,
+    $prompt
+);
